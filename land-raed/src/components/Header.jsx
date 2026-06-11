6386098287a525c1934a0daeb2c5b2c6ed84310b
Nimira43 <n.rabone72@aol.com>
2026-06-11 03:06:51 +0100

    Basic header added.

--- land-raed/src/components/Header.jsx
+++ land-raed/src/components/Header.jsx
@@ -1,8 +1,23 @@
+import { MdOutlineEmail, MdOutlineHomeWork, MdOutlinePhone } from 'react-icons/md'
+
 const Header = () => {
   return (
-    <div>
-      Header      
-    </div>
+    <header className='header'>
+      <div className='item brand'>
+        <MdOutlineHomeWork className='icon' />
+        <span className='logo-font'>
+          Land-rǣd
+        </span>
+      </div>
+      <div className='item contact'>
+        <MdOutlinePhone className='icon' />
+        <span>(0121) 123 4567</span>
+      </div>
+      <div className='item contact'>
+        <MdOutlineEmail className='icon' />
+        <span>enquires@landraed.com</span>
+      </div>
+    </header>
   )
 }
 
