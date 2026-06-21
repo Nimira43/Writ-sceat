ef21f3a9fe5f23fc19240f3c9ee1f40801a7aeaf
Nimira43 <n.rabone72@aol.com>
2026-06-21 06:37:27 +0100

    Footer

--- land-raed/src/components/Footer.jsx
+++ land-raed/src/components/Footer.jsx
@@ -1,8 +1,49 @@
+import { IoMdTime } from 'react-icons/io'
+
 const Footer = () => {
+  const openingHour = 9
+  const closingHour = 5
+  const now = new Date()
+  const currentHour = now.getHours()
+  const currentDay = now.getDay()
+  const isWeekday = currentDay >= 1 && currentDay <= 5
+  const isOpen = 
+    isWeekday &&
+    currentHour >= openingHour &&
+    currentHour <= closingHour
+
+  const openElement = (
+    <>
+      <div className='message'>
+        <IoMdTime className='icon' />
+        <span className='status open'>
+          We are now open.
+        </span>
+      </div>
+      <div className='footer-contact'>
+        Call us on (0121) 123 4567
+      </div>
+    </>
+  )
+  
+  const closedElement = (
+    <>
+      <div className='message'>
+        <IoMdTime className='icon' />
+        <span className='status closed'>
+          We are now closed.
+        </span>
+      </div>
+      <div className='footer-contact'>
+        Opening hours: Monday to Friday, {openingHour}am to {closingHour}pm
+      </div>
+    </>
+  )
+
   return (
-    <div>
-      Footer      
-    </div>
+    <footer className='footer'>
+      {isOpen ? openElement : closedElement}      
+    </footer>
   )
 }
 
